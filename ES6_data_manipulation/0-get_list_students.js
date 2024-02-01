8f6654be8ece3314eb1d9b2c8063ad8e1334e3de
peredemacron <peredemacron@gmail.com>
2024-02-01 22:47:47 +0100

    0 task fix

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,18 +1,11 @@
-const getListStudents = () => [
-  {
-    id: 1,
-    firstName: 'Guillaume',
-    location: 'San Francisco',
-  },
-  {
-    id: 2,
-    firstName: 'James',
-    location: 'Columbia',
-  },
-  {
-    id: 5,
-    firstName: 'Serena',
-    location: 'San Francisco',
-  },
-];
+function getListStudents() {
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+
+  return students;
+}
+
 export default getListStudents;
